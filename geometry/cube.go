a62e1d8987f94e9f8e3a5eaa87bc11140a08e829
jfgudk <j.gudknecht@elsevier.com>
2021-12-15 18:54:48 -0500

    Fat fingers again

--- geometry/cube.go
+++ geometry/cube.go
@@ -4,7 +4,7 @@ import "errors"
 
 func CubeVolume(n int) (int, error) {
 	if n != 0 {
-		return n * n * n, nill
+		return n * n * n, nil
 	}
 	return 0, errors.New("Zero length edge not allowed")
 }
